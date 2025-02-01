1abbb207508f766b8e68c57a03bcf0ea2f1ea110
Weldify <donotemailme@bro>
2025-02-01 10:20:59 +0100

    Kinda rethink the entire game

--- scripts/worldspawn.gd
+++ scripts/worldspawn.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 func _func_godot_build_complete():
 	for child in get_parent().get_children():
-		if child.name.ends_with("_func_geo"):
+		if child.name.ends_with("_func_geo") or child.name.ends_with("_func_detail"):
 			print("Gleeker")
 			child.add_to_group("navigation_mesh_source_group")
 		
